79ee4bf6f46bfad47ae4465bcf3c31da9bceaeb8
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-02-05 15:30:53 +0100

    VFS-5219 added page footer

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -1,4 +1,5 @@
 import contentQuery from './components/content-query';
+import pageFooter from './components/page-footer';
 import pageHeader from './components/page-header';
 import queryParameters from './components/query-parameters';
 import resourceLoadError from './components/resource-load-error';
@@ -6,6 +7,7 @@ import resourceLoadError from './components/resource-load-error';
 export default {
   components: {
     contentQuery,
+    pageFooter,
     pageHeader,
     queryParameters,
     resourceLoadError,
