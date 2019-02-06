3e0393228019aeea3f0abf375bea7da295e7f725
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-02-06 12:09:44 +0100

    VFS-5219 fixex testem config

--- src/testem.js
+++ src/testem.js
@@ -1,26 +1,12 @@
 module.exports = {
   framework: 'mocha',
   test_page: 'tests/index.html?hidepassed',
+  report_file: 'tests/test-results.xml',
   disable_watching: true,
   launch_in_ci: [
-    'Chrome',
+    'Firefox',
   ],
   launch_in_dev: [
-    'Chrome',
+    'Firefox',
   ],
-  browser_args: {
-    Chrome: {
-      ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
-        '--headless',
-        '--disable-gpu',
-        '--disable-dev-shm-usage',
-        '--disable-software-rasterizer',
-        '--mute-audio',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900',
-      ].filter(Boolean),
-    },
-  },
 };
