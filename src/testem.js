b5948a89269756ff74c31b8086de9ddd03268b1e
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-02-07 16:24:38 +0100

    VFS-5219 added option to not produce test report file

--- src/testem.js
+++ src/testem.js
@@ -1,7 +1,9 @@
+/* eslint-env node */
+
 module.exports = {
   framework: 'mocha',
   test_page: 'tests/index.html?hidepassed',
-  report_file: 'tests/test-results.xml',
+  report_file: process.env.NO_REPORT_FILE ? null : 'tests/test-results.xml',
   disable_watching: true,
   launch_in_ci: [
     'Firefox',
