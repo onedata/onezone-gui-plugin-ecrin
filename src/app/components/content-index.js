815740e950dd56a984cc5862c6e6319265efd37b
Michal Borzecki <michal.borzecki@hotmail.com>
2019-02-18 08:33:00 +0100

    VFS-5219 added dummy forward from index to query page

--- src/app/components/content-index.js
+++ src/app/components/content-index.js
@@ -1,11 +1,20 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 import I18n from 'onezone-gui-plugin-ecrin/mixins/i18n';
 
 export default Component.extend(I18n, {
   classNames: ['content-index'],
 
+  router: service(),
+
   /**
    * @override
    */
   i18nPrefix: 'components.contentIndex',
+
+  actions: {
+    find() {
+      this.get('router').transitionTo('query');
+    },
+  },
 });
--- src/app/locales/en/components/content-index.js
+++ src/app/locales/en/components/content-index.js
@@ -1,4 +1,5 @@
 export default {
   title: 'The Clinical Studies Data Objects Index',
   subtitle: 'Discover the datasets &amp; documents',
+  find: 'Find',
 };
