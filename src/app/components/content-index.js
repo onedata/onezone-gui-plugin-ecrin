e3ba0fa9bc97fd9dec4d4445eccf7329e656709a
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-03-12 16:59:23 +0100

    VFS-5319 added inputs to start page

--- src/app/components/content-index.js
+++ src/app/components/content-index.js
@@ -1,20 +1,62 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
 import I18n from 'onezone-gui-plugin-ecrin/mixins/i18n';
+import QueryParams from 'onezone-gui-plugin-ecrin/utils/query-params';
 
 export default Component.extend(I18n, {
   classNames: ['content-index'],
 
   router: service(),
+  configuration: service(),
 
   /**
    * @override
    */
   i18nPrefix: 'components.contentIndex',
 
+    /**
+   * @type {Array<string>}
+   */
+  modeOptions: Object.freeze([
+    'specificStudy',
+    'studyCharact',
+    'viaPubPaper',
+  ]),
+
+  /**
+   * @type {Ember.ComputedProperty<Array<Object>>}
+   */
+  studyIdTypeMapping: reads('configuration.studyIdTypeMapping'),
+
+  /**
+   * @type {Ember.ComputedProperty<Array<Object>>}
+   */
+  typeFilterOptions: reads('configuration.typeMapping'),
+
+  /**
+   * @type {Ember.ComputedProperty<Array<Object>>}
+   */
+  accessTypeFilterOptions: reads('configuration.accessTypeMapping'),
+
+  /**
+   * @type {Ember.ComputedProperty<Array<Object>>}
+   */
+  publisherFilterOptions: reads('configuration.publisherMapping'),
+
+  /**
+   * @type {Ember.ComputedProperty<Utils.QueryParams>}
+   */
+  queryParams: computed(function queryParams() {
+    return QueryParams.create();
+  }),
+
   actions: {
     find() {
-      this.get('router').transitionTo('query');
+      this.get('router').transitionTo('query', {
+        queryParams: this.get('queryParams.queryParams'),
+      });
     },
   },
 });
--- src/app/locales/en/components/content-index.js
+++ src/app/locales/en/components/content-index.js
@@ -1,5 +1,8 @@
-export default {
+import queryParameters from './query-parameters';
+import _ from 'lodash';
+
+export default _.assign({}, queryParameters, {
   title: 'The Clinical Studies Data Objects Index',
   subtitle: 'Discover the datasets &amp; documents',
   find: 'Find',
-};
+});
